4753a0dc5888181e332767040350f927aeb85c61
lyeejj <lyshine012@gmail.com>
2023-09-08 20:53:00 +0900

    feat: 상수 이름 변경 및 추가

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -5,8 +5,10 @@ export const ROUTES = {
 
 export const URL = 'http://localhost:4000/sick';
 
-export const MAX_LIST_NUM = 7;
+export const MAX_SEARCH_LIST_NUM = 7;
 
 export const CACHE_NAME = 'search';
 
 export const CACHE_DELETE_TIME = 30000;
+
+export const DEBOUNCE_DELAY_TIME = 500;
